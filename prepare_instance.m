2d6b725ac4bfb33ef63a20a6db229b4210a31cd0
Lukas Koller <lukas.koller@tum.de>
2025-05-11 10:54:57 +0200

    reset parameters

--- prepare_instance.m
+++ prepare_instance.m
@@ -68,11 +68,11 @@ function [nn,options,permuteDims] = aux_readNetworkAndOptions( ...
   % Compute the exact bounds of the constraint zonotope.
   options.nn.exact_conzonotope_bounds = false;
   % Specify number of splits, dimensions, and neuron-splits.
-  options.nn.num_splits = 3; 
+  options.nn.num_splits = 2; 
   options.nn.num_dimensions = 1;
-  options.nn.num_neuron_splits = 1;
+  options.nn.num_neuron_splits = 0;
   % Add relu tightening constraints.
-  options.nn.num_relu_constraints = 10;
+  options.nn.num_relu_constraints = 0;
 
   % Default: do not permute the input dimensions. 
   permuteDims = false;
